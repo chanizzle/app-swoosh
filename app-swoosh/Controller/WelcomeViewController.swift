71b0664cc10599b628bf5d0a406671825548abc5
Jasmine <jasmine.chan89@gmail.com>
2018-03-21 17:16:51 -0400

    fix bugs

---
+++ app-swoosh/Controller/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeViewController.swift
+//  app-swoosh
+//
+//  Created by JasmineChan on 2018-03-18.
+//  Copyright © 2018 chanizzle. All rights reserved.
+//
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
