bea353ebd432b9206709d7d6b9ac18b34f8f391a
Jasmine <jasmine.chan89@gmail.com>
2018-03-23 18:03:09 -0400

    passing data along

--- app-swoosh/Controller/LeagueViewController.swift
+++ app-swoosh/Controller/LeagueViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class LeagueViewController: UIViewController {
-
+    
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
     }
-    
+   
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillViewControllerSegue", sender: self)
     }
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    
     
 
 }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by JasmineChan on 2018-03-23.
+//  Copyright © 2018 chanizzle. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
